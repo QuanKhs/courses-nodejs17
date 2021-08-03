7d0304ec4a4ce0eed95b1274edb2f9c689c2c5d5
QuanKhs <quanphamluong@gmail.com>
2021-08-03 08:50:32 +0700

    sql table

--- database.sql
+++ database.sql
@@ -10,3 +10,31 @@ drop database if exists instagram_test;
 drop database if exists instagram;
 create database instagram;
 use instagram;
+
+-- delete table 
+drop table users;
+drop table if exists users;
+
+-- create table
+create table users(
+	id int,
+    firstName varchar(255),
+    lastName varchar(255),
+    age int,
+    birthday date
+);
+
+-- edit table column
+alter table users
+modify column firstName varchar(150),
+modify column lastName varchar(150);
+
+-- add column
+alter table users 
+add column roles varchar(100);
+
+-- delete coluum
+alter table users
+drop column roles;
+
+
