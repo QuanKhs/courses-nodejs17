24137db67d300c6825c78f85dd1e3198d1338add
QuanKhs <quanphamluong@gmail.com>
2021-08-03 08:23:25 +0700

    sql database

---
+++ database.sql
@@ -0,0 +1,12 @@
+-- create database
+CREATE DATABASE INSTAGRAM;
+create database instagram_test;
+
+-- delete database
+drop database INSTAGRAM;
+drop database if exists instagram_test;
+
+-- use database
+drop database if exists instagram;
+create database instagram;
+use instagram;
