18c353685a712ce0e7cfcfcf89a40482e69482ea
QuanKhs <quanphamluong@gmail.com>
2021-08-17 19:50:47 +0700

    sequelize cli

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('nodejs17_movies', 'root', '123456', {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql',
-});
+// const sequelize = new Sequelize('nodejs17_movies', 'root', '123456', {
+//     host: 'localhost',
+//     port: 3306,
+//     dialect: 'mysql',
+// });
 
-const initSequelize = async () => {
-    // sequelize.authenticate().then(() => {
-    //     console.log('Connection has been established successfully.');
-    // }
-    // ).catch(err => {
-    //     console.log('Unable to connect to the database:', err);
-    // }
-    // );
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (err) {
-        console.log('Unable to connect to the database:', err);
-    }
+// const User = sequelize.define('User', {
+//     name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     age: {
+//         type: DataTypes.INTEGER,
+//     }
+// });
 
-};
+// const syncModel = async () => {
+//     try {
+//         await User.sync({ alter: true });
+//         console.log('sync success');
+//     } catch (error) {
+//         console.log('sync error');
+//     }
+// };
 
-initSequelize();
+// syncModel();
+
+// const createUser = async () => {
+//     try {
+//         const user = await User.create({
+//             name: 'Jackson Pham',
+//             age: 18
+//         });
+//         console.log(user);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+// createUser();
+
+// const initSequelize = async () => {
+//     // sequelize.authenticate().then(() => {
+//     //     console.log('Connection has been established successfully.');
+//     // }
+//     // ).catch(err => {
+//     //     console.log('Unable to connect to the database:', err);
+//     // }
+//     // );
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//     } catch (err) {
+//         console.log('Unable to connect to the database:', err);
+//     }
+
+// };
+
+// initSequelize();
+
